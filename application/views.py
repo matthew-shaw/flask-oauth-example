44f369af637aa19a05eeef7e3c65f5ec184b2a8c
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-04-14 12:22:00 +0100

    fixes

--- application/views.py
+++ application/views.py
@@ -1,22 +1,22 @@
 from flask import render_template
 from application import app
 
-@app.route("/health")
+@app.route('/health', methods=["GET"])
 def check_status():
-    return "Status OK"
+    return status
 
-@app.route("/")
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
-@app.route("/signin")
+@app.route('/signin', methods=["GET"])
 def signin():
     return render_template('signin.html')
 
-@app.route("/signup")
+@app.route('/signup', methods=["GET"])
 def signup():
     return render_template('signup.html')
 
-@app.route("/profile")
+@app.route('/profile', methods=["GET"])
 def profile():
     return render_template('profile.html')
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-from application.views import app
+from application import app
 
 app.run(debug=True, host="0.0.0.0", port=5000)
